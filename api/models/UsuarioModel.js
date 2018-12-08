fcae974a76b835ca42551becf5a1695c853cf82e
marcus <demaroar@gmail.com>
2018-12-08 12:00:56 -0200

    modelo usuário

---
+++ api/models/UsuarioModel.js
@@ -0,0 +1,93 @@
+"use strict"
+
+class UsuarioModel {
+    constructor(nome, email, usuario, senha, cpf, isAdmin, ativo) {
+        this._validaDadosDoUsuario(nome, email, usuario, senha, cpf)
+        this._nome = nome;
+        this._email = email;
+        this._usuario = usuario;
+        this._senha = senha;
+        this._cpf = cpf;
+        this._isAdmin = isAdmin;
+        this._ativo = ativo;
+    }
+
+    _validaDadosDoUsuario(nome, email, usuario, senha, cpf)
+    {
+        if (!nome)
+            throw new Error("É necessário informar um nome para o usuário");
+        if (!email)
+            throw new Error("É necessário informar um email para o usuário");
+        if (!usuario)
+            throw new Error("É necessário informar um nome de usuário");
+        if (!senha)
+            throw new Error("É necessário informar uma senha para o usuário");
+        if (!cpf)
+            throw new Error("É necessário informar um cpf para o usuário");
+        if (!this._validaCPF(cpf))
+            throw new Error("CPF inválido.");
+    }
+    
+    _validaCPF(cpf) {
+        let Soma = 0;
+        let Resto;
+
+        if (cpf == "00000000000") return false;
+        
+        for (let i=1; i<=9; i++) 
+            Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+        Resto = (Soma * 10) % 11;
+    
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
+    
+        Soma = 0;
+        for (let i = 1; i <= 10; i++) 
+            Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
+        Resto = (Soma * 10) % 11;
+    
+        if ((Resto == 10) || (Resto == 11))  Resto = 0;
+        if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
+
+        return true;
+    }
+
+    desativar() {
+        this.ativo = false;
+        // TODO: persistir no banco de dados
+    }
+
+    get nome() {
+        return this._nome;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    get usuario() {
+        return this._usuario;
+    }
+
+    get senha() {
+        return this._senha;
+    }
+
+    get cpf() {
+        return this._cpf;
+    }
+
+    get isAdmin() {
+        return this._isAdmin;
+    }
+
+    get ativo() {
+        return this._ativo;
+    }
+
+    set ativo(value) {
+        throw new Error("Essa propriedade não pode ser alterada");
+    }
+}
+
+module.exports = UsuarioModel;
---
+++ tests/models/UsuarioTests.js
@@ -0,0 +1,102 @@
+var assert = require("assert");
+
+var Usuario = require("../../api/models/UsuarioModel.js");
+var cpfValido = "81627934022";
+
+beforeEach("Setting up the userList", () => {
+    
+});
+
+describe("Para testar os acessos básicos ao objeto usuário", () => {
+    var usuario;
+
+    beforeEach("Configura um usuário para ser testado", () => {
+        usuario = new Usuario("marcus", "marcus@contagem.bh", "usuario", "senha", cpfValido, true, true);
+    });
+    
+    it("deve verificar o acesso ao nome dele", () => {
+        assert.equal(usuario.nome, "marcus");
+    });
+
+    it("deve verificar o acesso ao email dele", () => {
+        assert.equal(usuario.email, "marcus@contagem.bh");
+    });
+
+    it("deve verificar o acesso ao nomde de usuario dele", () => {
+        assert.equal(usuario.usuario, "usuario");
+    });
+
+    it("deve verificar o acesso a senha dele", () => {
+        assert.equal(usuario.senha, "senha");
+    });
+
+    it("deve verificar o acesso ao cpf dele", () => {
+        assert.equal(usuario.cpf, cpfValido);
+    });
+
+    it("deve validar que o usuário é um administrador", () => {
+        assert.equal(usuario.isAdmin, true);
+    });
+
+    it("deve validar que esse usuário não é um administrador", () => {
+        let usuarioNaoAdmin = usuario = new Usuario("marcus", "marcus@contagem.bh", "usuario", "senha", "11111111111", false, true);
+        assert.equal(usuario.isAdmin, false);
+    });
+
+    it("deve validar que o usuário está ativo", () => {
+        assert.equal(usuario.ativo, true);
+    });
+
+    it("deve validar que esse usuário não está ativo", () => {
+        let usuarioNaoAtivo = usuario = new Usuario("marcus", "marcus@contagem.bh", "usuario", "senha", "11111111111", true, false);
+        assert.equal(usuario.ativo, false);
+    });
+
+    it("deve garantir que a propriedade 'ativo' não pode ser alterada se ela estiver falsa", () => {
+        assert.throws(() => usuario.ativo = false, Error);
+    });
+});
+
+describe("Para garantir que os usuários cadastrados no sistema terão CPFs válidos", () => {
+    it("o sistema deve garantir que esse usuário pode ser criado porque seu CPF é válido", () => {
+        let usuario = new Usuario("marcus", "marcus@contagem.bh", "usuario", "senha", cpfValido, true, true);
+        assert.equal(usuario.cpf, cpfValido);
+    });
+
+    it("o sistema não deve deixar esse usuário ser criado porque o CPF dele não é válido", () => {
+        assert.throws(() => new Usuario("marcus", "marcus@contagem.bh", "usuario", "senha", "00000000000", true, true), Error);
+    });
+});
+
+describe("Para validar que os dados cadastrais dos usuários são obrigatórios", () => {
+    it("deve garantir que um nome seja fornecido", () => {
+        assert.throws(() => new Usuario("", "marcus@contagem.bh", "usuario", "senha", cpfValido, true, true), Error);
+    });
+
+    it("deve garantir que um email seja fornecido", () => {
+        assert.throws(() => new Usuario("marcus", "", "usuario", "senha", cpfValido, true, true), Error);
+    });
+
+    it("deve garantir que um nome de usuário seja fornecido", () => {
+        assert.throws(() => new Usuario("marcus", "marcus@contagem.bh", "", "senha", cpfValido, true, true), Error);
+    });
+
+    it("deve garantir que uma senha seja fornecido", () => {
+        assert.throws(() => new Usuario("marcus", "marcus@contagem.bh", "usuario", "", cpfValido, true, true), Error);
+    });
+
+    it("deve garantir que um cpf seja fornecido", () => {
+        assert.throws(() => new Usuario("marcus", "marcus@contagem.bh", "usuario", "senha", null, true, true), Error);
+    });
+});
+
+describe("Quando um usuário for excluído", () => {
+    it("o sistema deve garantir que ele não está mais ativo após a exclusão", () => {
+
+        assert.equal(usuario._validaCPF(), "marcus");
+    });
+});
+
+
+
+
